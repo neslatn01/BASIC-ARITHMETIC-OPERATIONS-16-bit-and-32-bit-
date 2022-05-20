20d4a45070d859dfcb49cd898e5d6f4b3226076a
NESLA T N <90820009+neslatn01@users.noreply.github.com>
2022-05-20 16:49:08 +0530

    Create 32 BIT ADDITION.c

---
+++ 32 BIT ADDITION.c
@@ -0,0 +1,28 @@
+
+DATA SEGMENT
+LIST DD 12121212H,12121212H
+N3 DW ?
+N4 DW ?
+DATA ENDS
+CODE SEGMENT
+ASSUME CS :CODE;DS:DATA
+START:
+        MOV AX,DATA
+        MOV DS,AX
+        XOR AX,AX
+        MOV CL,AL
+        MOV AX,[SI]
+        ADD AX,[SI+4]
+        MOV BX,AX
+        MOV N3,BX
+        MOV AX,[SI+2]
+        ADD AX,[SI+6]
+        MOV DX,AX
+        MOV N4,DX
+        JNC STOP
+        INC CL
+STOP:
+        MOV AX,4CH
+        INT 21H
+CODE ENDS
+END START
